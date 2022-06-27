758db66dadc953146f6062a569c6a53ee55f5c96
Jason Patterson <jpatterson@MBP-13.local>
2022-06-26 20:59:30 -0600

    modified veos-config script

--- eos_scripts/build_veos-config.sh
+++ eos_scripts/build_veos-config.sh
@@ -10,16 +10,16 @@ event-handler test2
    action bash
       rm /mnt/flash/veos-config
       touch /mnt/flash/veos-config
-      chmod 755 /mnt/flash/veos-config
+      chmod 777 /mnt/flash/veos-config
       printf "%s\n" $'SERIALNUMBER='$HOSTNAME >> /mnt/flash/veos-config
       printf "%s\n" $'SYSTEMMACADDR=001c.73'$((RANDOM % 99))'.'$((RANDOM % 9999)) >> /mnt/flash/veos-config
       FastCli -c 'write memory' -p 15
       FastCli -c 'reload now' -p 15
       EOF
-   threshold 10 count 1
+   threshold 50 count 1
    !
    trigger on-counters
-      poll interval 8
+      poll interval 60
       condition bashCmd."if test -f /mnt/flash/veos-config; then tracker=1; else tracker=0; fi; echo $tracker" < 1
 
 
